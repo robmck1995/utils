a7b00ca028372b9f9ce6407cbbc211e5647c63b7
Robert McKnight <rmcknight@spacex.com>
2016-06-02 18:16:57 -0700

    Temp broke interest calc

--- interest_calc.py
+++ interest_calc.py
@@ -1,11 +1,15 @@
 import argparse
 
-parser = argparse.ArgumentParser(description="Calculate yield after n years of an interest rate x compounded annually")
+compounding_map = {"weekly" : 52, "monthly" : 12, "quarterly" : 4, "biannually" : 2}
+
+parser = argparse.ArgumentParser(description="Calculate yield after n years of an interest rate x")
 parser.add_argument("interest", type=float)
 parser.add_argument("years", type=int)
+parser.add_argument("initial_investment", type=float)
+parser.add_argument("--recurring", type=float)
+parser.add_argument("--period", choices=["weekly", "monthly", "quarterly", "biannually", "annually"], default="annually")
+parser.add_argument("--compounding", choices=["weekly", "monthly", "quarterly", "biannually", "annually"], default="annually")
 args = parser.parse_args()
 intr = args.interest
 years = args.years
-value = pow(1+intr, years)
-print "Assuming all returns are reinvested..."
-print "After " + str(years) + " years at " + str(intr*100) + "% interest, your money will be multiplied by: " + str(value) + "x"
+inital = args.initial_investment
